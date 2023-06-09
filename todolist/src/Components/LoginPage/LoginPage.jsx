6026a0d9018d29205b1b5d37181bc8a8b3be0fb5
sajjadhza <sa1ho3za77@gmail.com>
2023-06-09 21:02:53 +0430

    start with MUI

--- todolist/src/Components/LoginPage/LoginPage.jsx
+++ todolist/src/Components/LoginPage/LoginPage.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Button, Stack, Typography } from "@mui/material";
+import SendIcon from '@mui/icons-material/Send';
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <>
+      <Stack display={"block"} spacing={2}>
+        <Typography variant="h3" gutterBottom>
+          welcome to check tasks app
+        </Typography>
+        <Typography variant="body1">
+          this app has made to remind and check your different tasks
+        </Typography>
+        <Typography variant="subtitle1">
+          please login or sign up first
+        </Typography>
+        <Stack direction={"row"} spacing={2}>
+          <Button
+            variant="contained"
+            startIcon={<SendIcon/>}
+            color="error"
+          >
+            login
+          </Button>
+        </Stack>
+      </Stack>
+    </>
+  );
 }
